dba472642150a8f8ba22debff472c011798adc54
Gustavo Straub <49662792+GustavoStraub@users.noreply.github.com>
2022-08-22 15:00:30 -0300

    Mui init

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Typography variant="h1">Zé</Typography>;
+};
+
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,49 @@
+import { ThemeOptions } from "@mui/material/styles";
+
+const theme: ThemeOptions = {
+  typography: {
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
+    h1: {
+      fontWeight: 700,
+      fontSize: "56px",
+      lineHeight: "68.26px",
+    },
+    h2: {
+      fontWeight: 700,
+      fontSize: "48px",
+      lineHeight: "58.51px",
+    },
+    h3: {
+      fontWeight: 700,
+      fontSize: "40px",
+      lineHeight: "49px",
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: "32px",
+      lineHeight: "39px",
+    },
+    h5: {
+      fontWeight: 500,
+      fontSize: "24px",
+      lineHeight: "29.26px",
+    },
+    h6: {
+      fontWeight: 500,
+      fontSize: "20px",
+      lineHeight: "24px",
+    },
+    subtitle1: {
+      fontWeight: 500,
+      fontSize: "16px",
+      lineHeight: "19.5px",
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: "16px",
+      lineHeight: "19.5px",
+    },
+  },
+};
+
+export default theme;
